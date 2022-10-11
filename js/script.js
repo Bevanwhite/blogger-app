c31db1cea5edfefc5cf83e00104e4d3c0dbc4eb3
Bevan White <jefferybevan@gmail.com>
2022-10-11 16:36:15 +0530

    added the toggle button with javascript

--- js/script.js
+++ js/script.js
@@ -22,10 +22,29 @@ const closeNav=()=>{
 //     openNavBtn.style.display = 'inline-block';
 //     closeNavBtn.style.display = 'none';
 // }
-// else if (screen.width>1024){
+// if (screen.width>1024){
+//     navItems.element.style.display = 'flex';
 //     openNavBtn.style.display = 'none';
-//     closeNavBtn.style.display = 'none';
 // }
 
 openNavBtn.addEventListener('click', openNav);
 closeNavBtn.addEventListener('click', closeNav);
+
+const sidebar = document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show__sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide__sidebar-btn');
+
+const showSidebar = () =>{
+    sidebar.style.left='0';
+    showSidebarBtn.style.display = 'none';
+    hideSidebarBtn.style.display = 'inline-block';
+}
+
+const hideSidebar = () =>{
+    sidebar.style.left='-100%';
+    showSidebarBtn.style.display = 'inline-block';
+    hideSidebarBtn.style.display = 'none';
+}
+
+showSidebarBtn.addEventListener('click', showSidebar);
+hideSidebarBtn.addEventListener('click', hideSidebar);
